c3d25476fe35271c5eef510972b4541562388555
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-27 13:00:25 +0200

    Third milestone completed

---
+++ src/org/lessons/events/Concert.java
@@ -0,0 +1,57 @@
+package org.lessons.events;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Concert extends Event{
+
+    // Attributi
+    private LocalTime hour;
+    private BigDecimal price;
+
+    // Costruttore
+    public Concert (String title, LocalDate date, int totalSeats, LocalTime hour, BigDecimal price) {
+        super(title, date, totalSeats);
+        this.hour = hour;
+        this.price = price;
+    }
+
+    // Metodi
+    public LocalTime getHour() {
+        return hour;
+    }
+
+    public void setHour(LocalTime hour) {
+        this.hour = hour;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return formatDateTime(getDate(), getHour()) + " - " + getTitle() + " - " + formatPrice();
+    }
+
+    private String formatPrice() {
+        return String.format("%,.2f €", price);
+    }
+
+    private String formatDateTime(LocalDate date, LocalTime time) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        String formattedDate = date.format(dateFormatter);
+        String formattedTime = time.format(timeFormatter);
+
+        return formattedDate + " " + formattedTime;
+    }
+
+}
