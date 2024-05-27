4cae565d5b1c6c2cc8940702738181c96969d2f1
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-27 12:13:29 +0200

    Second milestone completed

--- src/org/lessons/events/Event.java
+++ src/org/lessons/events/Event.java
@@ -1,25 +1,66 @@
 package org.lessons.events;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Event {
 
     // Attributi
-    String title;
-    LocalDate date;
-    int totalSeats;
-    int bookedSeats;
+    private String title;
+    private LocalDate date;
+    private int totalSeats;
+    private int bookedSeats;
 
 
     // Costruttore
     public Event (String title, LocalDate date, int totalSeats){
         this.title = title;
-        this.date = date;
+        this.date = validateDate(date);
         this.totalSeats = totalSeats;
         bookedSeats = 0;
     }
 
     // Metodi
+    public void bookSeats(int seats) throws IllegalArgumentException{
+        if (seats <= 0){
+            throw new IllegalArgumentException("Invalid number of seats: " + seats);
+        }
+
+        if (bookedSeats + seats > totalSeats){
+            throw new IllegalArgumentException("Can't book this number of seats: " + seats);
+        }
+        bookedSeats += seats;
+    }
+
+    public void cancelBoooking(int seats) throws IllegalArgumentException{
+        if (seats <= 0){
+            throw new IllegalArgumentException("Invalid number of seats: " + seats);
+        }
+        if (bookedSeats - seats < seats){
+            throw new IllegalArgumentException("Can't cancel this number of seats: " + seats);
+        }
+        if (date.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Invalid date: " + date);
+        }
+        bookedSeats -= seats;
+    }
+    
+    private LocalDate validateDate(LocalDate date){
+
+        if (date.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Invalid date: " + date);
+        }else {
+            return date;
+        }
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MMMM dd", Locale.ENGLISH);
+        return date.format(formatter) + " - " + title;
+    }
+
     public String getTitle() {
         return title;
     }
--- src/org/lessons/events/Main.java
+++ src/org/lessons/events/Main.java
@@ -1,7 +1,79 @@
 package org.lessons.events;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String eventTitle = "";
+        LocalDate eventDate = null;
+        int totalEventSeats = 0;
+        int bookChoice = 0;
+        boolean exit = false;
+        int userChoice;
+        int cancelChoice;
+        int availableSeats;
 
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Whats the title of the event? ");
+            eventTitle = scanner.nextLine();
+
+            System.out.print("When will the event be held?(YYYY/MM/D) ");
+            eventDate = LocalDate.parse(scanner.nextLine());
+
+            System.out.print("Enter the number of seats for this event ");
+            totalEventSeats = Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e){
+            System.out.println("Error: " + e.getMessage());
+        } catch (DateTimeParseException e){
+            System.out.println("Invalid date: " + eventDate);
+        } catch (NullPointerException e){
+            System.out.println("Error " + e.getMessage());
+        }
+
+        if (!Objects.equals(eventTitle, "") || eventDate != null){
+            Event event = new Event(eventTitle, eventDate, totalEventSeats);
+
+            while(!exit){
+                System.out.print("Enter 1 to book seats, 2 to calcel booking, 3 exit ");
+                userChoice = Integer.parseInt(scanner.nextLine());
+
+                try {
+                    if (userChoice == 1){
+                        System.out.print("How many seats do you want to book? ");
+                        bookChoice = Integer.parseInt(scanner.nextLine());
+
+                        event.bookSeats(bookChoice);
+                    } else if (userChoice == 2){
+                        System.out.print("How many seats do you want to book? ");
+                        cancelChoice = Integer.parseInt(scanner.nextLine());
+
+                        event.cancelBoooking(cancelChoice);
+                    } else if (userChoice == 3){
+                        exit = true;
+                        System.out.println("Have a good day!");
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+
+            availableSeats = event.getTotalSeats() - event.getBookedSeats();
+
+            System.out.println("Title: " + event.getTitle());
+            System.out.println("Date: " + event.getDate());
+            System.out.println("Total seats: " + event.getTotalSeats());
+            System.out.println("Booked seats: " + event.getBookedSeats());
+            System.out.println("Available seats: " + availableSeats);
+
+        }else {
+            System.out.println("Error during the inizialization of the event");
+        }
+
+        scanner.close();
     }
 }
